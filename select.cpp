efd31814c5de66e9bb24e81126c17dd9a40ef2fe
Hayden Cowell <cowellh@os1.engr.oregonstate.edu>
2017-10-10 19:07:50 -0700

    Added all sorts of good stuff

--- select.cpp
+++ select.cpp
@@ -42,54 +42,11 @@ int getInt(ifstream &inFile, int pos){
 }
 
 int main(){
-	system("xxd -ps 1.dat 1.txt");
-	ifstream inFile("1.txt", ios::in);
+	system("xxd *.dat *.txt");
+	
+	//system("xxd -ps 1.dat 1.txt");
+	//ifstream inFile("1.txt", ios::in);
 	
 	for(int i=0; i<10; i++)
 	cout << getInt(inFile, 5) << endl;
 }
-
-
-/*int main() {
-	system("xxd -ps 1.dat 1.txt");
-	ifstream inFile("1.txt", ios::in);
- 
-char tempC;
-  int tempI;
-  int num = 0;
-
-  while(inFile){
-  num = 0;
-  for(int i = 1; i <= 8; i++){
-   inFile.get(tempC);
-   if(tempC == '\n')
-     inFile.get(tempC);
-
-   if(tempC == 'a')
-    tempI = 10;
-   else if(tempC == 'b')
-    tempI = 11;
-   else if(tempC == 'c')
-    tempI = 12;
-   else if(tempC == 'd')
-    tempI = 13;
-   else if(tempC == 'e')
-    tempI = 14;
-   else if(tempC == 'f')
-    tempI = 15;
-   for(int j = 0; j < 10; j++){
-     if((int)tempC == 48+j)
-       tempI = j;
-   }
-
-   //cout << i << ": " << tempI << endl;
-   num = num + (tempI * pow( 16 , (8 - i) )); 
-  }
-
-  cout << num << endl;
-}
-
-  return 0;
-}*/
-
-
